2b3cb917d2158b403e322b608cd2429232befb06
Lincoln Bryant <lbryant@hubspot.com>
2014-12-08 11:01:09 -0500

    fix node

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-require("6to5/register");
+var System = require('es6-module-loader').System;
+
 require("colors");
 console.log('Welcome to ES6 in node.  ES5 modules are supported alongside E6!'.green);
-require("./src/main.js");
+
+System.import('./src/main').then(function(m) {
+  console.log(m.p);
+});
