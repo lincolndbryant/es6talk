7a196471715f040a616c78eb4f5623b6b99f7ea7
Lincoln Bryant <lbryant@hubspot.com>
2014-12-08 11:30:34 -0500

    useless statement

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ var System = require('es6-module-loader').System;
 require("colors");
 console.log('Welcome to ES6 in node.  ES5 modules are supported alongside E6!'.green);
 
-System.import('./src/main').then(function(m) {
-  console.log(m.p);
-});
+System.import('./src/main')
